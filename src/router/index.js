e03159812381e7d45ae1c7b3e5e26a02110b7092
Shawn Karber <smkarber@gmail.com>
2023-03-09 14:48:35 -0600

    Add 404 route + page

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,10 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('@/views/AboutView.vue')
+    },
+    {
+      path: '/:catchAll(.*)',
+      component: () => import('@/views/NotFoundView.vue')
     }
   ]
 });
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>404: The requested resource does not exist.</h3>
+</template>
