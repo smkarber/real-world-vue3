e8ee7225973f31ea1110f7afb28c211e71189781
Shawn Karber <smkarber@gmail.com>
2023-03-11 02:57:10 -0600

    Build out pagination

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,13 +1,24 @@
 <script setup>
-  import { onMounted, ref } from 'vue';
+  import { computed, ref, watchEffect } from 'vue';
   import EventCard from '@/components/EventCard.vue';
   import EventService from '@/services/EventService';
 
-  const events = ref(null);
+  const props = defineProps({ page: { type: Number } });
 
-  onMounted(() => {
-    EventService.getEvents()
-      .then((resp) => (events.value = resp.data))
+  const events = ref(null);
+  const totalEvents = ref(0);
+
+  const hasNextPage = computed(() => {
+    return props.page < Math.ceil(totalEvents.value / 2);
+  });
+
+  watchEffect(() => {
+    events.value = null;
+    EventService.getEvents(2, props.page)
+      .then((resp) => {
+        events.value = resp.data;
+        totalEvents.value = resp.headers['x-total-count'];
+      })
       .catch((err) => console.error(`Axios error fetching events: ${err}`));
   });
 </script>
@@ -16,6 +27,20 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :event="event" :key="event.id" />
+    <div class="pagination">
+      <RouterLink
+        id="previous-link"
+        v-if="page != 1"
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        >&#60; Previous</RouterLink
+      >
+      <RouterLink
+        id="next-link"
+        v-if="hasNextPage"
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        >Next &#62;</RouterLink
+      >
+    </div>
   </div>
 </template>
 
@@ -24,5 +49,25 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  .pagination {
+    display: flex;
+    flex-direction: row;
+    width: 290px;
+  }
+
+  .pagination a {
+    flex: 1;
+    text-decoration: none;
+    color: #2c3e50;
+  }
+
+  #previous-link {
+    text-align: left;
+  }
+
+  #next-link {
+    text-align: right;
   }
 </style>
